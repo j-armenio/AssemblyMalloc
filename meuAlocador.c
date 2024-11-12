fa7b636be6e2f24ce0d95842eb283ac4bd213aed
j-armenio <joaoarmeniosilveira@gmail.com>
2024-11-12 12:00:33 -0300

    alocaMem feito, mas quero mudar umas coisas ainda

--- meuAlocador.c
+++ meuAlocador.c
@@ -2,23 +2,28 @@
 #include <stdio.h>
 #include "meuAlocador.h"
 
-void* topoInicialHeap;
+void* topoInicialHeap = NULL;
+void* topoAtualHeap = NULL;
 
-// Armazena o endereço do topo corrente da heap                             
-void iniciaAlocador() 
+// Armazena o endereço do topo corrente da heap e armazena em topoInicialHeap
+void iniciaAlocador()
 {
     topoInicialHeap = sbrk(0);
+    topoAtualHeap = topoInicialHeap;
     return;
 }
 
 // Restaura o valor original da heap
 void finalizaAlocador() 
 {
-    brk(topoInicialHeap);
+    if (brk(topoInicialHeap) == 0)
+        printf("Heap restaurada com sucesso.\n");
+    else
+        printf("Erro ao restaurar heap.\n");
     return;
 }
 
-// Indica que
+// Devolve para heap o bloco alocado
 int liberaMem(void* bloco) 
 {
     void* p = bloco - 16 * 8;
@@ -27,8 +32,54 @@ int liberaMem(void* bloco)
     return 0;
 }
 
+// Bloco:
+// | estado(8 bytes) | tamanho(8 bytes) | dados(até 4096 bytes) |
+
+// Aloca um bloco com num_bytes na heap e retorna o endereço inicial desse bloco
 void* alocaMem(int num_bytes)
 {
+    if (num_bytes <= 0) {
+        printf("num_bytes inválido.\n");
+        return NULL;
+    }
 
+    // Ajusta o tamanho solicitado para incluir metadados
+    int tamAlocar = num_bytes + 16;
+
+    // Procura melhor bloco livre (best-fit)
+    void* melhorBloco = NULL;
+    void* atual = topoInicialHeap;
+    int menorTamanho = __INT_MAX__;
+
+    while (atual < topoAtualHeap) {
+        int estado = *(int*)atual;        // Estado
+        int tamanho = *((int*)(atual+8)); // Tamanho
+
+        if (estado == 0 && tamanho >= tamAlocar && tamanho < menorTamanho) {
+            melhorBloco = atual;
+            menorTamanho = tamanho;
+        }
+
+        // Move para o proximo bloco
+        atual += tamanho;
+    }
+
+    // Se encontrou um bloco adequado
+    if (melhorBloco) {
+        *(int*)melhorBloco = 1; // Marca como ocupado
+        return melhorBloco + 16; // REtorna endereço útil do bloco
+    }
+    
+    // Se não encontrou expande a heap
+    int tamanhoNovoBloco = ((tamAlocar + 4095) / 4096) * 4096;
+    void *novoBloco = sbrk(tamanhoNovoBloco);
+    if (novoBloco == (void*)-1) return NULL;
+
+    *(int*)novoBloco = 1; // Marca como ocupado
+    *((int*)(novoBloco + 8)) = tamanhoNovoBloco; // Tamanho do bloco
+
+    // Atualiza topo da heap
+    topoAtualHeap = novoBloco + tamanhoNovoBloco;
+
+    return novoBloco + 16;
 }
-
